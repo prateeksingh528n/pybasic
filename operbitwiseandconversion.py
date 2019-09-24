cca4825c3b5f3719475ed9c898b17f8ad3364888
prateeksingh528n <52025469+prateeksingh528n@users.noreply.github.com>
2019-09-24 23:06:04 +0530

    oper bittwise and binary decimal hexa conversion
    examples

---
+++ operbitwiseandconversion.py
@@ -0,0 +1,36 @@
+print('ps')
+print(bin(25))
+#binary to decimal above expMPLE
+#dec to bin
+print(0b011101)
+print(oct(25))
+print(hex(10))
+print(0xf)
+print(0b11001)
+
+#bitwise complement and or left shift and right shift
+
+print(~12)
+print(~45)
+#bitwise and or
+
+print(12 & 13)#bitwise and
+print(12 | 13)#bitwise or
+print(25 & 30)
+#xor operation
+
+print(12 ^ 13 #xor operation)
+
+#left shift and right shift
+
+
+
+
+
+
+
+
+
+
+
+
